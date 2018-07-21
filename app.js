fc7d602e7c801aa6de4ec0bd12c769b06f5e4026
Garik Nurmurodoff <garikn95@gmail.com>
2018-07-21 17:55:29 +0500

    Deleted mongoose connection references

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const TeleBot = require('telebot');
-// const mongoose = require('mongoose');
 const bot = new TeleBot('339371115:AAEOSgOwRGXgndDMs1LF4VtjZF86vuNU0s8');
-
-// const { dbURL, dbName } = require('./config');
 const func = require('./funcs');
-
-// mongoose.connect(dbURL + '/' + dbName);
 
 bot.on('text', (msg) => {    
     if(msg.text === "/start")
